e41e557d0fc148b7f1761a606124245c7d869df3
SandyLeiva <sandy.leiva@tecsup.edu.pe>
2023-09-21 02:18:36 -0500

    First Commit

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -4,5 +4,6 @@ public class StudentController {
 	public static void main( String[] args )
     {
         System.out.println( "Hola soy Carmen!" );
+        System.out.println( "Hola soy Sandy!" );
     }
 }
